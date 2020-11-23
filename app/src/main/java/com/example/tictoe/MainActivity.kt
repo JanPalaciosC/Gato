d0beece1d5ee4833b55cae740054412b7e9dab96
JanPalaciosC <phobosentured@gmail.com>
2020-11-23 15:37:37 -0700

    Implementacion de JuegoLocal
    Se agrego la plantilla de diseño del juego del gato, mas su activity con sus argumentos logicos(NO COMPLETADO), aun se necesita lograr la funcionalidad

--- app/src/main/java/com/example/tictoe/MainActivity.kt
+++ app/src/main/java/com/example/tictoe/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.tictoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         Thread.sleep(2000)
@@ -11,5 +17,19 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+            button.setOnClickListener(){
+                startActivity(Intent(this, JuegoLocalActivity::class.java))
+        }
+
     }
+
+
+
 }
+
+
+
+
